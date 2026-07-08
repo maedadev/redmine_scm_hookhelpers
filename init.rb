a7e1e234e18f23f5be47e6fcce77b6377ca42479
y-sugawara <y.sugawara@bizside.biz>
2026-07-08 12:02:12 +0900

    Remove to_prepare from init.rb and define plugin namespace

--- init.rb
+++ init.rb
@@ -5,11 +5,11 @@ Redmine::Plugin.register :redmine_scm_hookhelpers do
   author 'Lukas Pirl'
   author_url 'http://lukas-pirl.de/'
   description 'This Plugin provides helpers for commit hooks when using the Redmine SCM plugin.'
-  version '0.2.0'
+  version '0.3.0'
   url 'https://github.com/lpirl/redmine_scm_hookhelpers'
-  requires_redmine :version_or_higher => '3.4.2'
+  requires_redmine :version_or_higher => '5.0'
 end
 
-Rails.configuration.to_prepare do
-  SysController.send(:include, SysControllerPatch)
+unless SysController.included_modules.include? RedmineScmHookhelpers::SysControllerPatch
+  SysController.send(:include, RedmineScmHookhelpers::SysControllerPatch)
 end
---
+++ lib/redmine_scm_hookhelpers/sys_controller_patch.rb
@@ -0,0 +1,39 @@
+module RedmineScmHookhelpers
+    module SysControllerPatch
+
+        def self.included(base)
+            base.extend(ClassMethods)
+            base.send(:include, InstanceMethods)
+            base.class_eval do
+                alias_method :fetch_changesets_without_by_root_url, :fetch_changesets
+                alias_method :fetch_changesets, :fetch_changesets_with_by_root_url
+            end
+        end
+
+        module ClassMethods
+        end
+
+        module InstanceMethods
+
+            def fetch_changesets_with_by_root_url
+                unless params[:root_url]
+                    return fetch_changesets_without_by_root_url
+                end
+
+                repositories = Repository.where(root_url: params[:root_url].to_s)
+
+                unless repositories
+                    head :not_found
+                end
+
+                for repository in repositories
+                    repository.fetch_changesets
+                    Rails.logger.info "fetched changesets for " + repository.root_url
+                end
+
+                head :ok
+            end
+
+        end
+    end
+end
